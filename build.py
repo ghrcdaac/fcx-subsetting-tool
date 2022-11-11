742c03025bf43f3c14660114c0dbd493e60465b0
Sanjog <st0128@uah.edu>
2022-11-11 11:50:47 -0600

    added build.py script that generates the zip of each subset type

---
+++ build.py
@@ -0,0 +1,23 @@
+import subprocess
+
+SUBSETTING_TYPES = ['CRS', 'FEGS', 'GLM', 'LIP', 'LIS', 'LMA']
+
+def execsc(command):
+    subprocess.Popen(f"{command}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+# create a dist dir. Inside the dist dir.
+execsc("mkdir ./dist")
+# for each subsetting type, create a new dir.
+for subsetting_type in SUBSETTING_TYPES:
+    newDir = f"{subsetting_type}_build"
+    execsc(f"mkdir ./dist/{newDir}")
+    # copy each subsetting type module into the new dir.
+    execsc(f"cp -r ./{subsetting_type} ./dist/{newDir}")
+    # copy helpers into the new dir.
+    execsc(f"cp -r ./helpers ./dist/{newDir}")
+    # also copy the lambda_function to the new dir.
+    execsc(f"cp ./lambda_function.py ./dist/{newDir}")
+    # also copy the zip to the new dir.
+    execsc(f"cp ./create_zip.sh ./dist/{newDir}")
+    # then create a zip for hte subsetting type
+    execsc(f"bash ./dist/{newDir}/create_zip.sh {subsetting_type}")
