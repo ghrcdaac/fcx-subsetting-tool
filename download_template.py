897d79a7475bca010c868473e9ef399797e81b25
Sanjog <st0128@uah.edu>
2022-11-18 12:56:42 -0600

    changed soruce dirs. Also changed download location to home/user/downloads in unix and linux

--- download_template.py
+++ download_template.py
@@ -6,15 +6,16 @@ import os
 import boto3
 from pathlib import Path 
 
-s3bucket = 'ghrc-fcx-viz-output'
+s3bucket = 'szg-ghrc-fcx-viz-output'
 AWSregion= 'us-east-1' 
 s3 = boto3.resource('s3', region_name=AWSregion)
 bucket = s3.Bucket(s3bucket)
 
-subDir = 'subsets/RHPeR8073622/'
+subDir = 'subsets/subset-221118121928-f2a0493f-8769-4ff5-9b84-3927f8d03660/'
 subset = subDir.split('subsets/')[1]
 
-localDir = 'tmp/'+subset #<--suset to be saved under local tmp/
+downloadDir = str(os.path.join(Path.home(), "Downloads")); # only for linux/unix
+localDir = downloadDir+'/'+subset #<--suset to be saved under local tmp/
 Path(localDir).mkdir(parents=True, exist_ok=True)
 
 for obj in bucket.objects.filter(Prefix=subDir):
@@ -22,13 +23,14 @@ for obj in bucket.objects.filter(Prefix=subDir):
     s3path, filename = os.path.split(obj.key)
 
     if(s3path+'/' != subDir):
+        # if file inside another dir
         Dir = s3path.split('/')[-1]
         Path(localDir+Dir).mkdir(parents=True, exist_ok=True)
         print('*Downloading '+os.path.join(subset,Dir,filename)+'....')
-        
         bucket.download_file(obj.key, os.path.join(localDir,Dir,filename))
     
     elif(obj.key != subDir):
         print('*Downloading '+os.path.join(subset,filename)+'....')
         bucket.download_file(obj.key, os.path.join(localDir,filename))
 
+print(f"Download complete. Check '{localDir}'")
