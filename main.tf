57e523479da2593c1ddcdcba1b90c5b618023467
Sanjog <st0128@uah.edu>
2023-05-15 11:02:48 -0500

    add rest api gateway for subset trigger.

--- main.tf
+++ main.tf
@@ -519,3 +519,33 @@ resource "aws_cloudwatch_log_group" "Subset_Trigger" {
 
   retention_in_days = 5
 }
+
+
+
+## REST API GATEWAY
+
+resource "aws_api_gateway_rest_api" "subset_trigger_api" {
+  name = "fcx-subsetting-trigger-api"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part = aws_lambda_function.subset_trigger.function_name #/name
+  parent_id   = aws_api_gateway_rest_api.subset_trigger_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.subset_trigger_api.id
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.subset_trigger_api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.subset_trigger_api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.subset_trigger.invoke_arn
+}
