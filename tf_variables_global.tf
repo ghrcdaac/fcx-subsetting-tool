e781931278791d2a313415edf3ef43b369172250
Sanjog <st0128@uah.edu>
2023-05-26 13:20:00 -0500

    changes to deployment method and readme

--- tf_variables_global.tf
+++ tf_variables_global.tf
@@ -4,14 +4,12 @@ variable "aws_creds_path" {
   description = "The path to aws credentials file"
 
   type    = string
-  default = "/home/sanjog/.aws/credentials"
 }
 
 variable "aws_region" {
   description = "AWS region for all resources."
 
   type    = string
-  default = "us-east-1"
 }
 
 
@@ -83,7 +81,6 @@ variable "fcx-sst-marshmallow_json" {
 
 variable "accountId" {
   type    = string
-  default = "307493436926"
 }
 
 variable "stage_name" {
