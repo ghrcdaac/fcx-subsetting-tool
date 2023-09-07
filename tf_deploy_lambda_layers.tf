c122a12deef8e0906223e76813d811211625f517
Sanjog <st0128@uah.edu>
2023-09-07 12:47:58 -0500

    removed zipping comments. modified source code hash to directly calculate from zipped layers.

--- tf_deploy_lambda_layers.tf
+++ tf_deploy_lambda_layers.tf
@@ -6,13 +6,6 @@ variable "lambda_layer_ws_client" {
   default = "websocket_client"
 }
 
-# # zip the lambda code
-# data "archive_file" "lambda_ws_client" {
-#   type = "zip"
-#   source_dir  = "${path.module}/lambda_layers/websocket_client"
-#   output_path = "${path.module}/dist/${var.lambda_layer_ws_client}.zip"
-# }
-
 # upload the zipped lambda code to the s3 bucket created
 resource "aws_s3_object" "lambda_ws_client" {
   bucket = aws_s3_bucket.lambda_bucket.id
@@ -27,7 +20,7 @@ resource "aws_lambda_layer_version" "ws_client" {
   s3_bucket = aws_s3_bucket.lambda_bucket.id
   s3_key = aws_s3_object.lambda_ws_client.key
   compatible_runtimes = ["python3.8"]
-  source_code_hash = data.archive_file.lambda_ws_client.output_base64sha256
+  source_code_hash = "${filebase64sha256("${path.module}/lambda_layers/${var.lambda_layer_ws_client}/${var.lambda_layer_ws_client}.zip")}"
 }
 
 
@@ -40,13 +33,6 @@ variable "lambda_layer_marshmallow_json" {
   type    = string
   default = "marshmallow_json"
 }
-
-# # zip the lambda code
-# data "archive_file" "lambda_marshmallow_json" {
-#   type = "zip"
-#   source_dir  = "${path.module}/lambda_layers/marshmallow_json"
-#   output_path = "${path.module}/dist/${var.lambda_layer_marshmallow_json}.zip"
-# }
 
 # upload the zipped lambda code to the s3 bucket created
 resource "aws_s3_object" "lambda_marshmallow_json" {
@@ -62,7 +48,7 @@ resource "aws_lambda_layer_version" "marshmallow_json" {
   s3_bucket = aws_s3_bucket.lambda_bucket.id
   s3_key = aws_s3_object.lambda_marshmallow_json.key
   compatible_runtimes = ["python3.8"]
-  source_code_hash = data.archive_file.lambda_marshmallow_json.output_base64sha256
+  source_code_hash = "${filebase64sha256("${path.module}/lambda_layers/${var.lambda_layer_marshmallow_json}/${var.lambda_layer_marshmallow_json}.zip")}"
 }
 
 
@@ -75,13 +61,6 @@ variable "lambda_layer_xarr_s3fs_h5ncf" {
   type    = string
   default = "xarr_s3fs_h5ncf"
 }
-
-# # zip the lambda code
-# data "archive_file" "lambda_xarr_s3fs_h5ncf" {
-#   type = "zip"
-#   source_dir  = "${path.module}/lambda_layers/xarr_s3fs_h5ncf"
-#   output_path = "${path.module}/dist/${var.lambda_layer_xarr_s3fs_h5ncf}.zip"
-# }
 
 # upload the zipped lambda code to the s3 bucket created
 resource "aws_s3_object" "lambda_xarr_s3fs_h5ncf" {
@@ -97,7 +76,7 @@ resource "aws_lambda_layer_version" "xarr_s3fs_h5ncf" {
   s3_bucket = aws_s3_bucket.lambda_bucket.id
   s3_key = aws_s3_object.lambda_xarr_s3fs_h5ncf.key
   compatible_runtimes = ["python3.8"]
-  source_code_hash = data.archive_file.lambda_xarr_s3fs_h5ncf.output_base64sha256
+  source_code_hash = "${filebase64sha256("${path.module}/lambda_layers/${var.lambda_layer_xarr_s3fs_h5ncf}/${var.lambda_layer_xarr_s3fs_h5ncf}.zip")}"
 }
 
 
@@ -111,19 +90,12 @@ variable "lambda_layer_xarr_scipy" {
   default = "xarr_scipy"
 }
 
-# # zip the lambda code
-# data "archive_file" "lambda_xarr_scipy" {
-#   type = "zip"
-#   source_dir  = "${path.module}/lambda_layers/xarr_scipy"
-#   output_path = "${path.module}/dist/${var.lambda_layer_xarr_scipy}.zip"
-# }
-
 # upload the zipped lambda code to the s3 bucket created
 resource "aws_s3_object" "lambda_xarr_scipy" {
   bucket = aws_s3_bucket.lambda_bucket.id
   key    = "${var.lambda_layer_xarr_scipy}.zip"
   source = "${path.module}/lambda_layers/${var.lambda_layer_xarr_scipy}/${var.lambda_layer_xarr_scipy}.zip"
-  etag = filemd5(data.archive_file.lambda_xarr_scipy.output_path)
+  etag = filemd5("${path.module}/lambda_layers/${var.lambda_layer_xarr_scipy}/${var.lambda_layer_xarr_scipy}.zip")
 }
 
 # create layer
@@ -132,5 +104,5 @@ resource "aws_lambda_layer_version" "xarr_scipy" {
   s3_bucket = aws_s3_bucket.lambda_bucket.id
   s3_key = aws_s3_object.lambda_xarr_scipy.key
   compatible_runtimes = ["python3.8"]
-  source_code_hash = data.archive_file.lambda_xarr_scipy.output_base64sha256
+  source_code_hash = "${filebase64sha256("${path.module}/lambda_layers/${var.lambda_layer_xarr_scipy}/${var.lambda_layer_xarr_scipy}.zip")}"
 }
